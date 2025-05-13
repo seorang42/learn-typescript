0179a0b101f8dd819b4279a7a9046f4cb01251f5
seorang42 <seorang42@naver.com>
2025-05-14 01:58:26 +0900

    11-4 외부 라이브러리 사용하기

--- section11/src/index.tsx
+++ section11/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import _ from "lodash"; // type 정보가 제공되어야 사용 가능
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
